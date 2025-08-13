a50ec756e91737d7528f06099c340db6800f2fe2
CZY774 <yogaliuze@gmail.com>
2025-08-14 03:34:33 +0700

    Removes experimental server components config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
-  },
+  // experimental: {
+  //   serverComponentsExternalPackages: ["@prisma/client", "prisma"],
+  // },
 };
 
 export default nextConfig;
